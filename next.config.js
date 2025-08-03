b7c147f69a0de46eef2bbb3e2f37d2ca16666a6a
kurniawandwipras <114613701+kdpras00@users.noreply.github.com>
2025-08-04 02:16:53 +0700

    full update fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // ❌ Nonaktifkan ini agar API route bisa jalan
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
